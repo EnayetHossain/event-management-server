f7db2ff0bbbd58c5314ac7f9bce355c058bc481a
EnayetHossain <enayethossasin135@gmail.com>
2024-01-09 17:11:07 +0600

    login and registration user authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,23 +1,67 @@
+require("dotenv").config();
 const User = require("../model/user");
 const CustomError = require("../error/customError");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const signIn = (req, res) => {
-  res.status(200).json({ msg: "user login" });
+const signIn = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    throw new CustomError("Provide all credentials", 400);
+
+  const user = await User.findOne({ email });
+
+  if (!user) throw new CustomError("User not found", 403);
+
+  try {
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (user.email !== email || !isMatch)
+      return res
+        .status(401)
+        .json({ status: "Failed", error: "Email or password is not correct" });
+
+    // generate token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "2d",
+    });
+
+    res.status(200).json({ status: "Success", token });
+  } catch (error) {
+    res.status(400).json({ status: "Failed", error });
+  }
 };
 
+// register the user.
 const signUp = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, confirmPassword } = req.body;
   const result = await User.findOne({ email });
 
   if (result) throw new CustomError("User already exists with this email", 403);
 
+  if (!name || !email || !password || !confirmPassword)
+    throw new CustomError("All fields are required", 400);
+
+  if (password !== confirmPassword)
+    throw new CustomError("Password and confirm password doesn't match", 403);
+
   try {
-    const user = await User.create({ name, email, password });
-    res.status(201).json({ user });
+    // generate salt for password
+    const salt = await bcrypt.genSalt(12);
+    // hash the password
+    const hashedPassword = await bcrypt.hash(password, salt);
+    // create a user on the database
+    const user = await User.create({ name, email, password: hashedPassword });
+    // generate token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "2d",
+    });
+
+    res.status(201).json({ status: "Success", token });
   } catch (error) {
-    res
-      .status(400)
-      .json({ error: "All information must be provided properly" });
+    console.log(error);
+    res.status(400).json({ status: "Failed", error });
   }
 };
 
