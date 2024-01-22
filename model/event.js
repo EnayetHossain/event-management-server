945e1bf1121cefc1bdf447c6307622dcd2148f3d
EnayetHossain <enayethossasin135@gmail.com>
2024-01-22 18:49:55 +0600

    small changes

--- model/event.js
+++ model/event.js
@@ -4,7 +4,27 @@ const eventSchema = new mongoose.Schema(
   {
     title: {
       type: String,
-      require: true,
+      required: [true, "Title must be provided"],
+    },
+
+    description: {
+      type: String,
+      required: [true, "Description must be provided"],
+    },
+
+    eventDate: {
+      type: Date,
+      required: [true, "Event date is required"],
+    },
+
+    ticketPrice: {
+      type: Number,
+      required: [true, "Ticket price is required"],
+    },
+
+    totalTickets: {
+      type: Number,
+      required: [true, "Total tickets are required"],
     },
   },
   { timestamps: true }
